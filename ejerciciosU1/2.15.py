5444923fd74420395707fb923f0e66cce26328b5
Llavesuke <llavesukeprogram@gmail.com>
2023-10-05 19:55:55 +0200

    1-15

---
+++ ejerciciosU1/2.15.py
@@ -0,0 +1,16 @@
+print('------------------')
+print('CUENTA DE AHORROS RAFAEL ALBERTI')
+print('------------------')
+
+
+while True:
+    try:
+        depositado= float(input('Dinero depositado -> '))
+        for i in range (1,4):
+            print('Año', i, '->' , depositado+((i*depositado*0.04)))
+        break
+    except ValueError:
+
+        print('Número invalido')
+            
+        
