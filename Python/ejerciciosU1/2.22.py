b132db24c59f94678b19da688f866b5718515284
Llavesuke <llavesukeprogram@gmail.com>
2023-10-09 19:28:46 +0200

    1-27 + extra V2

--- Python/ejerciciosU1/2.22.py
+++ Python/ejerciciosU1/2.22.py
@@ -3,6 +3,6 @@ print('VOCAL MAYUSCULA RAFAEL ALBERTI')
 print('------------------') 
 
 
-frase= input('Introduce una frase y luego una vocal -> ')
-contador=int(len(frase))
-print(frase[0:contador-1]+frase[contador-1:contador].upper())
+frase= input('Introduce una frase ->')
+vocal= input('Vocal? ->')
+print(frase.replace(vocal, vocal.upper()))
