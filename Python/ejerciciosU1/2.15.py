f600313a4336cf89824cdbe374760af62b1d9767
Llavesuke <llavesukeprogram@gmail.com>
2023-10-09 19:04:27 +0200

    1-27 ejercicios + extra

--- Python/ejerciciosU1/2.15.py
+++ Python/ejerciciosU1/2.15.py
@@ -7,7 +7,7 @@ while True:
     try:
         depositado= float(input('Dinero depositado -> '))
         for i in range (1,4):
-            print('Año', i, '->' , depositado+((i*depositado*0.04)))
+            print('Año', i, '->' , '{:.2f}'.format(i*depositado*0.04))
         break
     except ValueError:
 
