f01a0e6a2b2e4b1525b6064e7543a4075cec12d4
llavesuke <llavesukeprogram@gmail.com>
2023-10-25 16:31:44 +0200

    cambio de nombre

--- Python/funciones/1.9-.5_def.py
+++ Python/funciones/1.9-.5_def.py
@@ -1,6 +1,8 @@
 
 def resultado(importe: float, iva: int):
     print(importe + (importe*(iva/100)))
+    importe= float(input('Introduce el importe sin iva -> '))
+    iva= int(input('Introduce el IVA a aplicar (ejemplo 10%) -> '))
 
 importe= float(input('Introduce el importe sin iva -> '))
 iva= int(input('Introduce el IVA a aplicar (ejemplo 10%) -> '))
